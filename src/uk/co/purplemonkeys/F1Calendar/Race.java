047aac16182ef2c22b25026b0d5b28c55db4a770
Leenookx <lnookx@googlemail.com>
2010-12-30 20:04:52 +0000

    Update the info for the Bahrain race.

--- src/uk/co/purplemonkeys/F1Calendar/Race.java
+++ src/uk/co/purplemonkeys/F1Calendar/Race.java
@@ -7,8 +7,9 @@ public class Race
 	private String race_name;
 	private Date race_date;
 	private String race_flag_filename;
+	private String race_session;
 	
-	public Race(String name, Date d, String flag_filename)
+	public Race(String name, Date d, String flag_filename, String session)
 	{
 		race_name = name;
 		race_date = d;
--- src/uk/co/purplemonkeys/F1Calendar/RaceCalendar.java
+++ src/uk/co/purplemonkeys/F1Calendar/RaceCalendar.java
@@ -9,7 +9,24 @@ public class RaceCalendar
 	public static void Initialise()
 	{
 		races.add( new Race("Bahrain", 
+								new Date(2011, 03, 11, 10, 00), 
+								"bahrain.jpg",
+								"Practice 1") );
+		races.add( new Race("Bahrain", 
+								new Date(2011, 03, 11, 14, 00), 
+								"bahrain.jpg",
+								"Practice 1") );
+		races.add( new Race("Bahrain", 
+								new Date(2011, 03, 12, 9, 00), 
+								"bahrain.jpg",
+								"Practice 3") );
+		races.add( new Race("Bahrain", 
+								new Date(2011, 03, 12, 13, 00), 
+								"bahrain.jpg",
+								"Qualifying") );
+		races.add( new Race("Bahrain", 
 								new Date(2011, 03, 13, 13, 00), 
-								"bahrain.jpg") );
+								"bahrain.jpg",
+								"Race") );
 	}
 }
