9a3cc6a58e4c5c62eee3b949fad3d9de619ef56f
Leenookx <lnookx@googlemail.com>
2010-12-30 12:36:19 +0000

    Starting to wend our merry way along development.

--- src/uk/co/purplemonkeys/F1Calendar/F1Calendar.java
+++ src/uk/co/purplemonkeys/F1Calendar/F1Calendar.java
@@ -1,13 +1,41 @@
 package uk.co.purplemonkeys.F1Calendar;
 
-import android.app.Activity;
-import android.os.Bundle;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Timer;
+import java.util.TimerTask;
 
-public class F1Calendar extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
-}
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.widget.RemoteViews;
+
+public class F1Calendar extends AppWidgetProvider
+{
+	        @Override
+	        public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+	                                                                   int[] appWidgetIds) {
+	               Timer timer = new Timer();
+	               timer.scheduleAtFixedRate(new MyTime(context, appWidgetManager), 1, 60000);
+	          }
+	          private class MyTime extends TimerTask {
+	                 RemoteViews remoteViews;
+	                 AppWidgetManager appWidgetManager;
+	                 ComponentName thisWidget;
+	                 public MyTime(Context context, AppWidgetManager appWidgetManager) {
+	                 this.appWidgetManager = appWidgetManager;
+	                 remoteViews = new RemoteViews(context.getPackageName(), R.layout.main);
+	                 thisWidget = new ComponentName(context, F1Calendar.class);
+	                 }
+	            @Override
+	            public void run() {
+	                        Date date1 = new Date();
+	                        Calendar calendar = new GregorianCalendar(2010, 11,25);
+	                        long days = (((calendar.getTimeInMillis()- date1.getTime())/1000))/86400;
+	                        remoteViews.setTextViewText(R.id.xmas,""+ days);
+	                        appWidgetManager.updateAppWidget(thisWidget, remoteViews);
+	             }
+	          }
+	        }
