7fee03f416050529e3eac62c88bcb5469321ac7d
Leenookx <lnookx@googlemail.com>
2010-12-30 13:35:39 +0000

    More updates.

---
+++ src/uk/co/purplemonkeys/F1Calendar/WidgetConfiguration.java
@@ -0,0 +1,13 @@
+package uk.co.purplemonkeys.F1Calendar;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WidgetConfiguration extends Activity
+{
+	@Override
+    public void onCreate(Bundle savedInstanceState)
+	{
+		// TODO: Implement configuration...
+	}
+}
