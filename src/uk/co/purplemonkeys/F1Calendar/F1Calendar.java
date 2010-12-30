93989610bba33846bc43219ca0cfeb9c92924f2b
unknown <richard@.(none)>
2010-12-30 10:32:15 +0000

    Initial project creation - skeleton only.

---
+++ src/uk/co/purplemonkeys/F1Calendar/F1Calendar.java
@@ -0,0 +1,13 @@
+package uk.co.purplemonkeys.F1Calendar;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class F1Calendar extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
