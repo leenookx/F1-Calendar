1d46dd14b29d709806374b2d0c6fd14c9f4b2500
Leenookx <lnookx@googlemail.com>
2010-12-30 12:55:26 +0000

    Is now compileable. If not actually doing anything! ;)

--- src/uk/co/purplemonkeys/F1Calendar/F1Calendar.java
+++ src/uk/co/purplemonkeys/F1Calendar/F1Calendar.java
@@ -12,30 +12,38 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.widget.RemoteViews;
 
+import uk.co.purplemonkeys.F1Calendar.R;
+
 public class F1Calendar extends AppWidgetProvider
 {
-	        @Override
-	        public void onUpdate(Context context, AppWidgetManager appWidgetManager,
-	                                                                   int[] appWidgetIds) {
-	               Timer timer = new Timer();
-	               timer.scheduleAtFixedRate(new MyTime(context, appWidgetManager), 1, 60000);
-	          }
-	          private class MyTime extends TimerTask {
-	                 RemoteViews remoteViews;
-	                 AppWidgetManager appWidgetManager;
-	                 ComponentName thisWidget;
-	                 public MyTime(Context context, AppWidgetManager appWidgetManager) {
-	                 this.appWidgetManager = appWidgetManager;
-	                 remoteViews = new RemoteViews(context.getPackageName(), R.layout.main);
-	                 thisWidget = new ComponentName(context, F1Calendar.class);
-	                 }
-	            @Override
-	            public void run() {
-	                        Date date1 = new Date();
-	                        Calendar calendar = new GregorianCalendar(2010, 11,25);
-	                        long days = (((calendar.getTimeInMillis()- date1.getTime())/1000))/86400;
-	                        remoteViews.setTextViewText(R.id.xmas,""+ days);
-	                        appWidgetManager.updateAppWidget(thisWidget, remoteViews);
-	             }
-	          }
-	        }
+	@Override
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) 
+	{
+		Timer timer = new Timer();
+		timer.scheduleAtFixedRate(new MyTime(context, appWidgetManager), 1, 60000);
+	}
+	
+	private class MyTime extends TimerTask 
+	{
+		RemoteViews remoteViews;
+		AppWidgetManager appWidgetManager;
+		ComponentName thisWidget;
+
+		public MyTime(Context context, AppWidgetManager appWidgetManager) 
+		{
+			this.appWidgetManager = appWidgetManager;
+			remoteViews = new RemoteViews(context.getPackageName(), R.layout.main);
+			thisWidget = new ComponentName(context, F1Calendar.class);
+		}
+	    
+		@Override
+	    public void run() 
+		{
+			Date date1 = new Date();
+			Calendar calendar = new GregorianCalendar(2010, 11,25);
+			long days = (((calendar.getTimeInMillis()- date1.getTime())/1000))/86400;
+			remoteViews.setTextViewText(R.id.xmas, "" + days);
+			appWidgetManager.updateAppWidget(thisWidget, remoteViews);
+		}
+	}
+}
